90007a7a805a96c13bdcb09e3c2d15f4bb9ce248
Georgi K <georgiknox@gmail.com>
2014-07-24 17:41:12 -0400

    parsed multi file length + file names

--- src/clj_torrent/core.clj
+++ src/clj_torrent/core.clj
@@ -1,9 +1,11 @@
 (ns clj-torrent.core
-  (:gen-class))
+  (:gen-class)
+  (:use clj-torrent.metadata))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (init-metadata "examples/flagfromserver.torrent"))
 
 
--- src/clj_torrent/metadata.clj
+++ src/clj_torrent/metadata.clj
@@ -6,6 +6,18 @@
   (let [file-contents (slurp filename :encoding "ISO-8859-1")]
     file-contents))
 
+(defn init-length [info]
+  (let [files (get info "files")]
+    (if (empty? files)
+      (get info "length")
+      (reduce (fn [accum file] (+ accum (get file "length"))) 0 files))))
+
+(defn init-files [info]
+  (let [files (get info "files")]
+    (if (empty? files)
+      (conj [] (get info "name"))
+      (into [] (map (fn [file] (clojure.string/join "/" (get file "path"))) files)))))
+
 (defn init-metadata
   [filename]
   (let [metadata (bencode/decode (read-torrent-file filename))
@@ -15,12 +27,19 @@
      :creation_date (get metadata "creation date")
      :encoding (get metadata "encoding")
      :info (bencode/encode "info")
-     :length (get info "length")
-     :files (get info "name")
+     :length (init-length info)
+     :files (init-files info)
      :piece_length (get info "piece length")
      :pieces (get info "pieces")}))
 
 
+;; testing
+(def sml-torrent (init-metadata "examples/flagfromserver.torrent"))
+(:files sml-torrent)
 
-(init-metadata "examples/flagfromserver.torrent")
+(init-metadata "examples/karl_marx.torrent")
 
+(def files [{"length" 291 "path" ["file" "path" "one.txt"]} {"length" 100 "path" ["file" "path" "two.txt"]}])
+(first files)
+(into [] (map (fn [file] (clojure.string/join "/" (get file "path"))) files))
+(reduce (fn [accum file] (+ accum (get file "length"))) 0 files)
